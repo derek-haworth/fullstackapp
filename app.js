af86a351eb4fff16f66b79b41e16e0141457ca6f
derek-haworth <derekhaw1@gmail.com>
2018-02-24 12:51:11 -0600

    create pages and update app.js with process.env

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require("express")
+var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -9,17 +10,21 @@ app.use(bodyParser.json());
 
 // Routes
 // =============================================================
-
 app.get("/", function(req, res) {
-
-  res.send("Initialized.")
-  // res.sendFile(path.join(__dirname, "view.html"));
+  // res.send("Initialized.")
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+
+app.get("api/all", function(req, res) {
+	console.log(req);
+	console.log(res);
+  res.send("data");
+});
+
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("I\'m listening... on port " + PORT);
 })
-
 
