b1d8885ea4fc11d8194c5fe131078693f3bf6b5c
derek-haworth <derekhaw1@gmail.com>
2018-02-24 13:17:30 -0600

    setup routing

--- app.js
+++ app.js
@@ -7,19 +7,53 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var waitListData = [];
+var tableData = [];
 
 // Routes
 // =============================================================
 app.get("/", function(req, res) {
-  // res.send("Initialized.")
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("api/all", function(req, res) {
-	console.log(req);
-	console.log(res);
-  res.send("data");
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+
+// API Routes
+// =============================================================
+app.get('/api/tables', function(req, res) {
+	res.json(tableData);
+});
+
+app.get('/api/waitlist', function(req, res) {
+	res.json(waitListData);
+});
+
+
+app.post('/api/tables', function(req, res) {
+	if(tableData.length < 5) {
+		tableData.push(req.body);
+		res.json(true);
+	} else {
+		waitListData.push(req.body);
+		res.json(false);
+	}
+});
+
+
+app.post('/api/clear', function(){
+	tableData = [];
+	waitListData = [];
+
+	console.log(tableData);
+	console.log(waitListData);
+})
 
 
 // Starts the server to begin listening
